f372ed287f9fe980d005983b2d3aba1487c8fc51
tanvir1017 <tanvir.hossaindev1@gmail.com>
2022-06-15 22:28:35 +0600

    added prisma db

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,12 +1,32 @@
 import type { LinksFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
+
 import stylesUrl from "~/styles/index.css";
+
 export const links: LinksFunction = () => {
-  return [{ rel: "StyleSheet", href: stylesUrl }];
+  return [
+    {
+      rel: "stylesheet",
+      href: stylesUrl,
+    },
+  ];
 };
+
 export default function Index() {
   return (
-    <div>
-      <h1>Welcome to Remix</h1>
+    <div className="container">
+      <div className="content">
+        <h1>
+          Remix <span>Jokes!</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="jokes">Read Jokes</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,12 +1,44 @@
-import { Outlet } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+import { Link, Outlet } from "@remix-run/react";
 
-const Jokes = () => {
-  return (
-    <div>
-      <h2>This is jokes part</h2>
-      <Outlet />
-    </div>
-  );
+import stylesUrl from "~/styles/jokes.css";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: stylesUrl }];
 };
 
-export default Jokes;
+export default function JokesRoute() {
+  return (
+    <div className="jokes-layout">
+      <header className="jokes-header">
+        <div className="container">
+          <h1 className="home-link">
+            <Link to="/" title="Remix Jokes" aria-label="Remix Jokes">
+              <span className="logo">🤪</span>
+              <span className="logo-medium">J🤪KES</span>
+            </Link>
+          </h1>
+        </div>
+      </header>
+      <main className="jokes-main">
+        <div className="container">
+          <div className="jokes-list">
+            <Link to=".">Get a random joke</Link>
+            <p>Here are a few more jokes to check out:</p>
+            <ul>
+              <li>
+                <Link to="some-joke-id">Hippo</Link>
+              </li>
+            </ul>
+            <Link to="new" className="button">
+              Add your own
+            </Link>
+          </div>
+          <div className="jokes-outlet">
+            <Outlet />
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
